bf63ea425dbe06225f3567044f78a4431dfaa52e
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-10 16:42:09 +0800

    created pseudo code

---
+++ print_numbers_from_0_to_100_except_numbers_ending_in_zero.py
@@ -0,0 +1,3 @@
+# Create a loop with an end of 101
+# Check if the iteration doesn't end with zero by using the modulo operator (%) and divisor of 10
+# If is doesn't end with zero, print the iteration
