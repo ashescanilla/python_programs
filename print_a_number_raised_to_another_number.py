2bd3d01eed61bd4ededde4407f3a1fb742f10ce4
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-10 16:12:05 +0800

    input added for the 2 numbers

--- print_a_number_raised_to_another_number.py
+++ print_a_number_raised_to_another_number.py
@@ -1,2 +1,5 @@
 # Ask for 2 number inputs
+num1 = int(input("Enter the first number (base): "))
+num2 = int(input("Enter the second number (exponent): "))
+
 # Print the result of raising the first number with the second number using (**)
