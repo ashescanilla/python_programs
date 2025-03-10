85cf72d916fc3f50243afddec04a23bf52ea4717
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-10 16:18:15 +0800

    added input for the 10 numbers

--- print_sum_of_10_numbers.py
+++ print_sum_of_10_numbers.py
@@ -1,2 +1,13 @@
 # Ask for 10 number inputs
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+num3 = int(input("Enter a number: "))
+num4 = int(input("Enter a number: "))
+num5 = int(input("Enter a number: "))
+num6 = int(input("Enter a number: "))
+num7 = int(input("Enter a number: "))
+num8 = int(input("Enter a number: "))
+num9 = int(input("Enter a number: "))
+num10 = int(input("Enter a number: "))
+
 # Print the sum by using the sum method
