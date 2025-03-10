621b8be2be2f0b2f6e5f0ec35d7768f1065c0226
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-10 15:38:32 +0800

    created comparison for 2 numbers

--- print_bigger_number_between_2_numbers.py
+++ print_bigger_number_between_2_numbers.py
@@ -4,4 +4,10 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 # Compare the 2 numbers
+bigger_number = None
+if num1 > num2:
+    bigger_number = num1
+elif num2 > num1:
+    bigger_number = num2
+
 # Print the bigger number
