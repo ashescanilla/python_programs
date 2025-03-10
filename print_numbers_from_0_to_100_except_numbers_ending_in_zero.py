a492a1c3409ecec3086b129ff6138da29e174c65
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-10 16:43:40 +0800

    checking if it ends with zero and printing if it does not

--- print_numbers_from_0_to_100_except_numbers_ending_in_zero.py
+++ print_numbers_from_0_to_100_except_numbers_ending_in_zero.py
@@ -1,4 +1,6 @@
 # Create a loop with an end of 101
 for i in range(101):
     # Check if the iteration doesn't end with zero by using the modulo operator (%) and divisor of 10
-    # If is doesn't end with zero, print the iteration
+    if i % 10 != 0:
+        # If is doesn't end with zero, print the iteration
+        print(i)
