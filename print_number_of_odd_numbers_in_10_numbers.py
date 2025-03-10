7a8be29ab085dd38d5a5b36d945ae3632dc27716
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-10 16:30:06 +0800

    variable initialized and input added for 10 numbers

--- print_number_of_odd_numbers_in_10_numbers.py
+++ print_number_of_odd_numbers_in_10_numbers.py
@@ -1,5 +1,16 @@
 # Initialize a variable to keep track of the count of odd numbers
+odd_count = 0
 # Ask for 10 number inputs
+num1 = int(input("Enter a number: "))
 # After an input check if the number is odd using the module operator (%)
 # If the number is odd, add 1 to the variable
+num2 = int(input("Enter a number: "))
+num3 = int(input("Enter a number: "))
+num4 = int(input("Enter a number: "))
+num5 = int(input("Enter a number: "))
+num6 = int(input("Enter a number: "))
+num7 = int(input("Enter a number: "))
+num8 = int(input("Enter a number: "))
+num9 = int(input("Enter a number: "))
+num10 = int(input("Enter a number: "))
 # Print the value of the variable after 10 inputs
