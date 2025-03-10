9569f8336305aa5721fadc536a0d017ee0bee50e
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-10 16:28:17 +0800

    created pseudo code

---
+++ print_number_of_odd_numbers_in_10_numbers.py
@@ -0,0 +1,5 @@
+# Initialize a variable to keep track of the count of odd numbers
+# Ask for 10 number inputs
+# After an input check if the number is odd using the module operator (%)
+# If the number is odd, add 1 to the variable
+# Print the value of the variable after 10 inputs
