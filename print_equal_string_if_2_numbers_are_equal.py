deb848bd5d35a6ab2c77045a692eb4862adafbc4
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-10 15:46:57 +0800

    added checking for equal inputs

--- print_equal_string_if_2_numbers_are_equal.py
+++ print_equal_string_if_2_numbers_are_equal.py
@@ -3,4 +3,5 @@ num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
 
 # Check if the 2 numbers are Equal
-# If the 2 numbers are equal, print "Equal"
+if num1 == num2:
+    # If the 2 numbers are equal, print "Equal"
