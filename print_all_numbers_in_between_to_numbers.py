e0ed39cae61f1a6728b6615fad2d7ee526ba8ba0
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 03:11:11 +0800

    Identifying the start and end of the range

--- print_all_numbers_in_between_to_numbers.py
+++ print_all_numbers_in_between_to_numbers.py
@@ -3,5 +3,8 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # Identify the start of the range with min() method plus 1 to exclude the inputted number
+start = min(num1, num2) + 1
 # Identify the end of the range with max() method
+end = max(num1, num2)
+
 # Print the range of numbers
