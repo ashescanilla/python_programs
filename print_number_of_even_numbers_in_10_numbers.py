0883528a0ba7aa80868f42a3e006b6cd8b79e464
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 02:42:49 +0800

    created pseudo code

---
+++ print_number_of_even_numbers_in_10_numbers.py
@@ -0,0 +1,5 @@
+# Create a variable to store the number of even number inputs and set its value to 0
+# Create a for loop to ask the user for 10 number inputs
+# Inside the loop, check if the number is even by using the modulus operator (%)
+# If the number is even, increment the counter variable by 1
+# After the loop, print the counter variable
