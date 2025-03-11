7d10a76adf1a8832cb5115a34b6d01b5ffd0193c
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 02:25:28 +0800

    checkin if division is possible

--- print_remainder_of_2_numbers.py
+++ print_remainder_of_2_numbers.py
@@ -3,5 +3,6 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 # Check if the second number is not equal to 0
-# Calculate the remainder of the two numbers using the modulus operator (%)
-# Print the remainder
+if num2 != 0:
+    # Calculate the remainder of the two numbers using the modulus operator (%)
+    # Print the remainder
