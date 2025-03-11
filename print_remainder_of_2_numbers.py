32b20b96a8ddb16139730f689883dbd8d4460557
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 02:25:02 +0800

    added inputs for 2 numbers

--- print_remainder_of_2_numbers.py
+++ print_remainder_of_2_numbers.py
@@ -1,4 +1,7 @@
 # Ask for 2 number inputs
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Check if the second number is not equal to 0
 # Calculate the remainder of the two numbers using the modulus operator (%)
 # Print the remainder
