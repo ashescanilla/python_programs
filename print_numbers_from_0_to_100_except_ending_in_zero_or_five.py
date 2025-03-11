039644f4911f3ba721ab7148785a48267ab7d20d
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 03:06:05 +0800

    added a for loop from 1 to 99

--- print_numbers_from_0_to_100_except_ending_in_zero_or_five.py
+++ print_numbers_from_0_to_100_except_ending_in_zero_or_five.py
@@ -1,3 +1,4 @@
 # Inititate a for loop with a start of 1, end of 100 (exclusive)
-# Check if the current number doesn't end with 5 or 0 using the modulo operator (%)
-# And the divisor is 5, if the number doesn't end with 5 or 0, print the number
+for i in range(1, 100):
+    # Check if the current number doesn't end with 5 or 0 using the modulo operator (%)
+    # And the divisor is 5, if the number doesn't end with 5 or 0, print the number
