e538c5b476abd51d7e82fcaf1bdd14a103e16743
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 02:36:30 +0800

    modified input message for the first input and create a loop to input 9 more numbers and subtract each input to the first number

--- print_difference_of_first_and_remaining_numbers.py
+++ print_difference_of_first_and_remaining_numbers.py
@@ -1,5 +1,9 @@
 # Ask for the first number
-num1 = int(input("Enter the first number: "))
+num1 = int(input("[1] Enter a number: "))
 
 # Create a loop, and ask for a number each time, subtract that number to the first number
+for i in range(2,11):
+    num = int(input(f"[{i}] Enter a number: "))
+    num1 -= num
+
 # Print the modified first number after the loop ends
