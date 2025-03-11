0d1f6878dcdbe0c7a2ed012dee34b8116a7322b2
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 02:25:52 +0800

    calculating the remainder

--- print_remainder_of_2_numbers.py
+++ print_remainder_of_2_numbers.py
@@ -5,4 +5,5 @@ num2 = int(input("Enter the second number: "))
 # Check if the second number is not equal to 0
 if num2 != 0:
     # Calculate the remainder of the two numbers using the modulus operator (%)
+    remainder = num1 % num2
     # Print the remainder
