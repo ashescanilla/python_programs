b2b455ce0c9174143cbfa12c4173aa7b4c62bae4
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 02:33:18 +0800

    added input for the first number

--- print_difference_of_first_and_remaining_numbers.py
+++ print_difference_of_first_and_remaining_numbers.py
@@ -1,3 +1,5 @@
 # Ask for the first number
+num1 = int(input("Enter the first number: "))
+
 # Create a loop, and ask for a number each time, subtract that number to the first number
 # Print the modified first number after the loop ends
