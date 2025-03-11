0c05908112cdc2e89cc4f3671eda08799e5ab116
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 02:53:28 +0800

    created pseudo code

---
+++ print_all_odd_numbers_from_0_to_100_using_while_loop.py
@@ -0,0 +1,4 @@
+# Create a variable for the current step with a value of 1
+# Skipping 0 since its an even number
+# Using a while loop, iterate over 1-100
+# Print the current step and increment its value by 2
