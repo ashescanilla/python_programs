515b14c1eb7b029307da03d5ecebca6c628cab1a
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 03:10:32 +0800

    added inputs for 2 numbers

--- print_all_numbers_in_between_to_numbers.py
+++ print_all_numbers_in_between_to_numbers.py
@@ -1,4 +1,7 @@
 # Ask for 2 numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
 # Identify the start of the range with min() method plus 1 to exclude the inputted number
 # Identify the end of the range with max() method
 # Print the range of numbers
