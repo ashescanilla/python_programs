3b787639636b41f0253af15cdbe59a3ad709189c
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 02:20:59 +0800

    floor dividing the first number over the second number

--- print_quotient_without_decimal_of_2_numbers.py
+++ print_quotient_without_decimal_of_2_numbers.py
@@ -5,4 +5,5 @@ num2 = int(input("Enter the second number: "))
 # Check if the second input is not zero
 if num2 != 0:
     # Floor divide the two numbers using (//) operator
+    result = num1 // num2
     # Print the result
