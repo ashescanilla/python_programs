e052e2a973275d90283d1d4caf58923be78f81b1
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 03:10:06 +0800

    created pseudo code

---
+++ print_all_numbers_in_between_to_numbers.py
@@ -0,0 +1,4 @@
+# Ask for 2 numbers
+# Identify the start of the range with min() method plus 1 to exclude the inputted number
+# Identify the end of the range with max() method
+# Print the range of numbers
