afe55439799337d9f30e58a74c9fcf7873535536
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 02:18:31 +0800

    added the inputs for 2 numbers

--- print_quotient_without_decimal_of_2_numbers.py
+++ print_quotient_without_decimal_of_2_numbers.py
@@ -1,3 +1,6 @@
 # Ask for 2 number inputs
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Floor divide the two numbers using (//) operator
 # Print the result
