5d4b36f70d06f7bdab35d3bd0435beacae531346
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 14:47:56 +0800

    comparing input with the current highest number and updating the highest number variable

--- print_highest_number_input.py
+++ print_highest_number_input.py
@@ -6,10 +6,13 @@ while True:
     try:
         # Convert the input to an integer
         user_input = int(input("Enter a number: "))
-    # If successful, check if the input is greater than the stored highest number
-    # If yes, update the highest number
+        # If successful, check if the input is greater than the stored highest number
+        if highest_number is None or user_input > highest_number:
+            # If yes, update the highest number
+            highest_number = user_input
+            
     except ValueError:
         # If input is invalid (non a number), break out of the loop
         break
-    
+
 # Print the highest number
