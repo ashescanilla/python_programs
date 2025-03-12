086ea46054714389df06c79749b1282cf0db871c
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 14:51:20 +0800

    created pseudocode

---
+++ print_numbers_in_descending_order.py
@@ -0,0 +1,3 @@
+# Continuously prompt the user for numeric input until an invalid entry is detected.
+# Stop taking input on invalid entry
+# Print the numbers in descending order if there is at least one valid entry.
