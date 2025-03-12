aac6d6a76e6b12d4193671f119dcfd57f0f7449e
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 14:42:30 +0800

    created pseudocode

---
+++ print_highest_number_input.py
@@ -0,0 +1,8 @@
+# Create a variable to store the highest number
+# Create an infinite loop with a while True statement
+# Inside the loop, ask the user for a number input
+# Convert the input to an integer
+# If successful, check if the input is greater than the stored highest number
+# If yes, update the highest number
+# If input is invalid (non a number), break out of the loop
+# Print the highest number
