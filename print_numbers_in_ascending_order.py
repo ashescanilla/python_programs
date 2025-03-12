8c28f69276d0cb87c729006fd0ef97b068f5aa8b
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 14:06:51 +0800

    add input loop to collect numbers until an invalid entry

--- print_numbers_in_ascending_order.py
+++ print_numbers_in_ascending_order.py
@@ -1,2 +1,13 @@
 # Continuously prompt the user for numeric input until an invalid entry is detected.
+print("Enter numbers (input anything non-numeric to stop)")
+
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break  # Stop taking input on invalid entry
+    
 # Print the numbers in ascending order if there is at least one valid entry.
