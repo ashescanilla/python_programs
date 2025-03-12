04d4b2cc27086d4de2fb61d09c0ea190b75af24b
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 14:35:43 +0800

    tracking the count of the number input throught the dictionary

--- print_number_with_most_number_of_inputs.py
+++ print_number_with_most_number_of_inputs.py
@@ -3,17 +3,22 @@ numbers = {}
 # Using a while True loop
 while True:
     # Try asking the user for a number and convert it into an integer
+    # If the user enters a valid integer value
     try:
         number = int(input("Enter a number: "))
-        # If the user enters a valid integer value
         # Check if the number exists in the dictionary
-        # If the number exists increase its input count by 1, 
-        # otherwise add the number to the dictionary with an input count of 1
+        if number in numbers:
+            # If the number exists increase its input count by 1, 
+            numbers[number] += 1
+        else:
+            # otherwise add the number to the dictionary with an input count of 1
+            numbers[number] = 1
+            
     # If the user enters an invalid integer value,
     except ValueError:
         # break out of the while loop
         break
-    
+
 # Create a variable to store the number with the highest input count, and set its current value to "None"
 # Iterate through the dictionary and identify the number with the highest input count
 # Check if there were any valid inputs by checking if the variable is not "None"
