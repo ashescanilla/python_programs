ecc3375abedfb92422e9d035aa59d11279d91275
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 11:37:20 +0800

    created list numbers to store user input

--- print_non_duplicate_numbers_from_10_inputs.py
+++ print_non_duplicate_numbers_from_10_inputs.py
@@ -1,4 +1,6 @@
 # Initialize an empty list to store user input
+numbers = []
+
 # Ask the user to input 10 numbers
 # Initialize a dictionary to track how many times each number appears
 # Record the frequency of each number in the list
