8e911dabd98eacc0dc8dcfda06b27402eec5a087
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 11:27:27 +0800

    created pseudocode

---
+++ print_non_duplicate_numbers_from_10_inputs.py
@@ -0,0 +1,5 @@
+# Initialize an empty list to store user input
+# Ask the user to input 10 numbers
+# Initialize a dictionary to track how many times each number appears
+# Record the frequency of each number in the list
+# Display numbers that appear only once
