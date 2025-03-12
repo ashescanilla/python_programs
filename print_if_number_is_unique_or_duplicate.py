0298741a569d8a97de1dd280fef5d520596cbccb
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 13:04:39 +0800

    start input loop and prompt user for a number

--- print_if_number_is_unique_or_duplicate.py
+++ print_if_number_is_unique_or_duplicate.py
@@ -1,6 +1,10 @@
 # Set to store unique numbers
 numbers = set()
 
+while True:
+    try:
     # Ask user for a number
+    num = int(input("Enter a number: "))
+
     # Check if the number is unique or a duplicate
     # Exit the loop if an invalid input is entered
