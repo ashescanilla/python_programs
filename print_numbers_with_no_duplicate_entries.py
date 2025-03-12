da0697be20102bcbe07904e58415d14d2fde818f
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-12 12:28:04 +0800

    initialize list and set for user number input

--- print_numbers_with_no_duplicate_entries.py
+++ print_numbers_with_no_duplicate_entries.py
@@ -1,3 +1,10 @@
 # Ask the user to input 10 numbers
+print("Enter 10 numbers")
+numbers = []
+seen = set()
+
+for _ in range(10):
+    num = int(input("Enter a number: "))
+    
 # Add the number only if it hasn't been entered previously
 # Print the numbers while ignoring duplicate entries  
