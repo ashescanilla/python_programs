6ba34875ed4d6ba5971034eeec51c630f72e6d03
ashescanilla <joannaashleyescanilla@gmail.com>
2025-03-21 16:11:23 +0800

    created pseudocode

---
+++ print_string_in_pascal_case.py
@@ -0,0 +1,4 @@
+# Ask the user to enter their name
+# Use the 'title()' method to capitalize the first letter of each word in the name
+# Use the 'strip()' method to remove the leading and trailing spaces from the name
+# Print the formatted name
